c4379a4cba6e929c80a485418753fc1a0b8abf55
jorgelinadev <jorgelinalberti@gmail.com>
2022-07-08 08:39:08 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,10 @@
+//alert ("Mi archivo está bien linkeado")
+
+const base = prompt ('Ingresa el valor de la base de tu triángulo en cm');
+const altura = prompt ('Ingresa el valor de la altura de tu triángulo en cm')
+
+const resultado = (base * altura ) / 2;
+
+alert ('El área de tu triángulo es de '+ resultado + ' centímetros cuadrados.');
+
+console.log (alert);
